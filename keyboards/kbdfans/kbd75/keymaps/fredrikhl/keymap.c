faff9fb45e07f584b365af30c3b0ec831ae27223
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-09-19 18:17:40 +0200

    Move KC_APP from RSHIFT

--- keyboards/kbdfans/kbd75/keymaps/fredrikhl/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/fredrikhl/keymap.c
@@ -7,6 +7,10 @@ enum custom_keycodes {
   COMP_OE,
 };
 
+/*
+ * TODO: Tap L_SFT: Shift lock or Caps lock?
+ * TODO: Tap R_SFT: Tap KC_APP
+ */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -14,9 +18,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_HOME,  KC_PGUP,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_END,   KC_PGDN,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PSCR,
-    VIM_L,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_SLCK,
-    KC_LSFT,  MO(2),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_APP,             KC_UP,    KC_PAUS,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   MO(1),    KC_BSPC,                      KC_RALT,  KC_CAPS,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    VIM_L,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_CAPS,
+    KC_LSFT,  MO(2),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_APP,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   MO(1),    KC_BSPC,                      KC_RALT,  KC_RGUI,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [1] = LAYOUT(
@@ -29,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT(
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  COMP_AA,  _______,  _______,            _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  COMP_OE,  COMP_AE,                      _______,  _______,
