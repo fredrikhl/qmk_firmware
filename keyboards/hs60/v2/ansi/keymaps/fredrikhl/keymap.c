b31ede232cc72a11ed4d67719643494c5a7215a3
Fredrik Larsen <fredrikhl@beef.lan>
2020-05-04 15:38:19 +0200

    Add missing keys (INS, APP)
    This commit re-organizes the pgup/pgdn cluster to add KC_INS/KC_DEL, and
    replaces KC_RALT with KC_APP (for use as a compose key)

--- keyboards/hs60/v2/ansi/keymaps/fredrikhl/keymap.c
+++ keyboards/hs60/v2/ansi/keymaps/fredrikhl/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
     KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
-    KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RALT,  SH_L1,    SH_L2,    KC_RCTL
+    KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_APP,   SH_L1,    SH_L2,    KC_RCTL
 ),
 
 /* FN layer */
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* board settings */
 [_LAYER2] = LAYOUT_60_ansi(
     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,
-    _______,  XXXXXXX,  KC_PGUP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  KC_HOME,  KC_PGDN,  KC_END,   XXXXXXX,  XXXXXXX,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,            _______,
+    _______,  KC_HOME,  KC_PGUP,  KC_END,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,  KC_INS,   KC_PGDN,  KC_DEL,   XXXXXXX,  XXXXXXX,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,            _______,
     _______,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            _______,
     _______,  _______,  _______,                                _______,                                _______,  SH_L3,    _______,  _______
 ),
